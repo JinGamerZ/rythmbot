8b24cbe1691f3e037a0c651c85dbd5908345a17c
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-03-27 22:33:10 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+// Discord.js bot
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+});
+
+client.on('message', msg => {
+    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
+    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
+    const args = msg.content.split(' ').slice(1).join(' ');
+    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
+    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+});
+
+// MESSAGES
+client.on('message', message => {
+    if (message.content === '?creadores') {
+    	message.reply('Wiim esta creado por @JinGamerZ Y @SrWitherHD.');
+  	}
+});
+
+client.login(process.env.TOKEN);
